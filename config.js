e033f67f5f09535d3374b41108709148abf99c28
unknown <johnathan.monsalve@udea.edu.co>
2018-11-04 00:18:33 -0500

    first commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/efrenApp'
+    db: process.env.MONGODB || 'mongodb://<johnathanmb95>:<johnathanmb95>@ds161099.mlab.com:61099/econtacts-database'
 }
