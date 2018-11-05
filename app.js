8022a088a8ea4cf0bc545444627f47381751e109
unknown <johnathan.monsalve@udea.edu.co>
2018-11-05 01:07:05 -0500

    try static

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(formidable.parse({keepExtensions: true}));
+app.use(express.static('public'));
 app.use('/api', api);
 
 
